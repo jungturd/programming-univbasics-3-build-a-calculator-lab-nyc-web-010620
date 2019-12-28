fdc7f398b167acd3f3b8c97e24164bd9f5d08f7c
Jeemin Shim <jeemin.shim@gmail.com>
2019-12-28 00:47:17 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,19 +3,15 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  difference = num1 - num2
-  puts difference
-  difference
+  num1 - num2
 end
 
 def division(num1, num2)
-  quotient = num1 / num2
-  puts quotient
-  quotient
+  num1 / num2
 end
 
 def multiplication(num1, num2)
-  product = num1 * num2
+    product = num1 * num2
   puts product
   product
 end
